0a675c81c271d2c3600e484f2b6a0057e55de26a
JimmyGx6 <jaime92_92@hotmail.com>
2023-02-15 14:10:16 +0100

    Moved translation files out of the code thanks to the i18next backend plugin

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,8 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -10,34 +12,6 @@ i18n
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
-    },
-    resources: {
-      en: {
-        translation: {
-          description: {
-            part1: "Edit src/App.js and save to reload.",
-            part2: "Learn React",
-          },
-        },
-      },
-      es: {
-        translation: {
-          description: {
-            part1:
-              "Edite src/App.js y guarde para recargar.",
-            part2: "Aprende React",
-          },
-        },
-      },
-      fr: {
-        translation: {
-          description: {
-            part1:
-              "Modifiez src/App.js et enregistrez pour recharger.",
-            part2: "Apprendre React",
-          },
-        },
-      },
     },
   });
 
