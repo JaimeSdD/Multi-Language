e309c400534158e3e360c96502a0b03596c9efed
JimmyGx6 <jaime92_92@hotmail.com>
2023-02-15 14:53:45 +0100

    Added greeting depending on the time of day

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
+import { DateTime } from 'luxon'
 import React from 'react'
+
+const greeting = (d = DateTime.now()) => {
+    const afternoon = 12;
+    const evening = 17;
+    const currentHour = parseFloat(d.toFormat("HH"));
+
+    if (currentHour >= afternoon && currentHour <= evening) {
+		return 'afternoon';
+	} else if (currentHour >= evening) {
+		return 'evening';
+  } 
+      return 'morning';
+}
 
 const Footer = ({t}) => {
   return (
     <div>
-        <div>{t('footer.date', { date: new Date() })}</div>
+        <div>{t('footer.date', { date: new Date(), context: greeting() })}</div>
     </div>
   )
 }
